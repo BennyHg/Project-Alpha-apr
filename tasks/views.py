fc84f552f78613a20429b1c6d1d2b294542fd3fd
benny.huang <bhuang997@gmail.com>
2022-06-19 14:03:35 -0700

    Feature 17 complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
+from audioop import reverse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
 from tasks.models import Task
@@ -20,12 +21,21 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
         job = form.save(commit=False)
         job.members = self.request.user
         job.save()
-        return redirect("show_project", pk=job.id)
+        return redirect("show_my_tasks")
 
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "tasks/task_list.html"
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ["is_completed"]
+    success_url = reverse_lazy("show_my_tasks")
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
